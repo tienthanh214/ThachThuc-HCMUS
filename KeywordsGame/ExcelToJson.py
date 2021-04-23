43560be393771d0f2a0426499bd262fc6cbe56be
tienthanh214 <39771202+tienthanh214@users.noreply.github.com>
2021-04-24 01:16:46 +0700

    Add files via upload

--- KeywordsGame/ExcelToJson.py
+++ KeywordsGame/ExcelToJson.py
@@ -1,15 +1,38 @@
 import pandas
 import json
+import xlsxwriter
+
+data_link = 'E:\GoogleDrive\ThachThuc\Keywords-Contribute.xlsx'
+
+# updated_index = pandas.read_excel(data_link, engine = 'openpyxl', usecols = ["UpdatedIndex"]).dropna()
+
+# idx = int(updated_index['UpdatedIndex'][0])
+
+excel_data_df = pandas.read_excel(data_link, engine = 'openpyxl').dropna(how = 'all')
+
+idx = int(excel_data_df['UpdatedIndex'][0])
+print("Old length: ", idx)
+
+if (idx >= len(excel_data_df)):
+    print("You're already newest data")
+    exit()
+
+json_str = excel_data_df[['Keyword']][idx:].to_json(orient = 'records')
+print(json_str)
+print(excel_data_df[idx:])
+
+idx += len(excel_data_df['Keyword'][idx:])
+print("New length: ", idx)
+
+excel_data_df.update({'UpdatedIndex' : [idx]})
+
+writer = pandas.ExcelWriter(data_link, engine = 'xlsxwriter')
+excel_data_df.to_excel(writer, sheet_name = 'Sheet1', header = True, index = False)
+writer.save()
 
 
 
-excel_data_df = pandas.read_excel('E:\GoogleDrive\ThachThuc\Keywords-Contribute.xlsx', engine = 'openpyxl', usecols = ["Keyword"]).dropna()
-
-json_str = excel_data_df.to_json(orient = 'records')
-
-
-# print(json_str)
-# print(len(excel_data_df))
+# update new records to data_base
 
 data = json.loads(json_str)
 link = 'C:/Users/ninhh/Downloads/auto-py-to-exe-master/auto-py-to-exe-master/output/keywords/keywords_.json'
