1e9c3826fd74921f73d3ed05badaf44c8ef1b255
ThanhNDT <thanhndt@vng.com.vn>
2023-03-27 17:04:45 +0700

    feat: update browser to hashrouter for locally run

--- NoiMangToanCau/training-tool/src/index.js
+++ NoiMangToanCau/training-tool/src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-	<BrowserRouter>
+	<HashRouter>
 		<React.StrictMode>
 			<App />
 		</React.StrictMode>
-	</BrowserRouter>
+	</HashRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
